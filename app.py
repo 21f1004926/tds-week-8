1e8991d5f2e21b0e9a775ecf28054c5a31a37dcf
21f1004926 <143320016+21f1004926@users.noreply.github.com>
2023-08-27 15:21:56 +0530

    Update app.py
    Redundant statements removed

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import pandas as pd
 import numpy as np
 
 st.write("""
-# Assignment App
+# Assignment (Find the largest among the 3 given numbers) App
 
 TDS Week 8
 """)
 #Get Input
 
-st.header('User Input Parameters')
+st.header('Select 3 numbers')
 
 def user_input():
     first_number = st.number_input("FIRST_NUMBER", min_value=0.0,max_value=2000000000.0)
@@ -20,9 +20,6 @@ def user_input():
     return data
 
 data = user_input()
-
-st.subheader('User Input parameters')
-st.write(data)
 
 st.subheader('The biggest number is:')
 st.write(np.max(data))
