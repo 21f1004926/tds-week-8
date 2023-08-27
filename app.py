b5cdbbbfa624a6a04df0295c5742ca7b349dbcab
21f1004926 <143320016+21f1004926@users.noreply.github.com>
2023-08-27 15:18:16 +0530

    Update app.py
    data converted to array

--- app.py
+++ app.py
@@ -10,27 +10,18 @@ TDS Week 8
 
 st.header('User Input Parameters')
 
-def user_input_features():
+def user_input():
     first_number = st.number_input("FIRST_NUMBER", min_value=0.0,max_value=2000000000.0)
     second_number = st.number_input("SECOND_NUMBER", min_value=0.0,max_value=2000000000.0)
     third_number = st.number_input("THIRD_NUMBER", min_value=0.0,max_value=2000000000.0)
 
-    data = {'FIRST_NUMBER': first_number,
-            'SECOND_NUMBER': second_number,
-            'THIRD_NUMBER': third_number
-            }
-    features = pd.DataFrame(data, index=[0])
-    return features
+    data = [ first_number, second_number, third_number ]
+    return data
 
-df = user_input_features()
+data = user_input()
 
 st.subheader('User Input parameters')
-df_dict = df.to_dict()
-st.write(df_dict)
-
-for col in df.columns:
-    if df[col].dtype != 'float64':
-        df[col] = df[col].values.astype('float64')
+st.write(data)
 
 st.subheader('The biggest number is:')
-st.write(df_dict['FIRST_NUMBER'])
+st.write(np.max(data))
